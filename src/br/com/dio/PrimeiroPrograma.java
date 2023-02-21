280c08ec5f511bb4ca8a5aa5c2c3544c74a500e5
Jefferson <jeffersonsgoncalves@gmail.com>
2023-02-21 16:40:40 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+	}
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
